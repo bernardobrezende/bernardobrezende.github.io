2585483ef274002d1ab572295214a6cc03c6cf03
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-08-11 13:37:40 -0300

    feat(tabs): add initial tabs.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,24 @@
 'use strict';
 
-(function(d) {
-  d.getElementById('copyYear').innerText = new Date().getFullYear();
+(function($d) {
+  $d.getElementById('copyYear').innerText = new Date().getFullYear();
+  var links = $d.querySelectorAll('#menu li a');
+  links.forEach(function(l) {
+    l.onclick = function(c) {
+      makeTabActive(l.getAttribute('href'));
+    }
+  });
+  var makeTabActive = function(tab) {
+    links.forEach(function(f) {
+      var href = f.getAttribute('href');
+      if (href !== tab) {
+        $d.querySelector(href).style.display = 'none';
+        f.style.fontWeight = '';
+      } else {
+        f.style.fontWeight = 'bold';
+        $d.querySelector(href).style.display = '';
+      }
+    });
+  }
+  makeTabActive('#about');
 })(document);
